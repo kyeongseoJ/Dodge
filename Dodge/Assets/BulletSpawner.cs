363038a68d71546219f5b497550c07733011535c
kyeongseoJ <kyeongseojeon@gmail.com>
2022-02-25 17:30:12 +0900

    20220225_Last

--- Dodge/Assets/BulletSpawner.cs
+++ Dodge/Assets/BulletSpawner.cs
@@ -7,11 +7,26 @@ public class BulletSpawner : MonoBehaviour
     // 생성할 탄알의 원본 프리팹 
     public GameObject bulletPrefab;
 
+    // 최소 생성 주기
+    public float spawnRateMin = 0.5f;
+    // 최대 생성 주기
+    public float spawnRateMax = 3f;
+    
+    // 실제 생성 주기 
+    private float spawnRate;
+    // 최근 생성 시점에서 지난 시간
+    private float timeAfterSpawn; 
+
     // 발사할 대상 : 변수선언
     private Transform target;
-    
+
     void Start()
     {
+        //최근 생성 이후에 누적시간을 0으로 초기화
+        timeAfterSpawn = 0;
+        // 탄알 생성 간격을 spawnRateMin과 spawnRateMax사이에서 랜덤 값 지정
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+
         // PlayerController 컴포넌트를 가진 게임 오브젝트를 찾아서 그 오브젝트의 위치값을 가져오란 의미
         target = FindObjectOfType<PlayerController>().transform;
     }
@@ -19,6 +34,8 @@ public class BulletSpawner : MonoBehaviour
     
     void Update()
     {
-        
+        // bullet 생성 초당 60개씩 생성되고 있다.
+        Instantiate(bulletPrefab);
+
     }
 }
