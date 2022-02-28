14eb4d276b8d6e528ed08d6d6eba442dba81a8b6
kyeongseoJ <kyeongseojeon@gmail.com>
2022-02-28 16:47:19 +0900

    20220228 Second

--- Dodge/Assets/02.Scripts/Rotator.cs
+++ Dodge/Assets/02.Scripts/Rotator.cs
@@ -9,6 +9,6 @@ public class Rotator : MonoBehaviour
     void Update()
     {
         // Rotate 메서드 : 입력값(매개변수)로  X, Y, Z 축에 대한  회전값을 받고 현재 회전상태에서 입력된 값만큼 상대적으로 더 회전한다.
-        transform.Rotate(0f, rotationSpeed, 0f);
+        transform.Rotate(0f, rotationSpeed*Time.deltaTime, 0f);
     }
 }
